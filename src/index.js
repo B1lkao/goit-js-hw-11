77f3d6fcd55abd306b42f57a2245b5d251fcead7
B1lkao <hrabousolesia@gmail.com>
2022-12-24 17:33:06 +0200

    Доробила дз

--- src/index.js
+++ src/index.js
@@ -1,79 +1,58 @@
-const loader=document.querySelector('[alt="loading"]')
-import Notiflix from 'notiflix';
+import './sass/index.scss';
 import NewAPIService from './new-service';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-const gallery = document.querySelector('.gallery');
-const axios = require('axios').default;
-const form = document.querySelector('.search-form');
+import axios from "axios";
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://pixabay.com/api/' ;
+const KEY = 'key=30951910-62deaf9663a2ad8fd5a993571';
+const options = 'image_type=photo&orientation=horizontal&horizontal=true';
+const perPage = 40;
+let value = '';
 let page = 1;
-const MAIN_URL_WITH_KEY = 'https://pixabay.com/api/?key=30629726-597c78df0089c177162f75c58';
 
-async function getUser() {
-  return await axios.get(`${MAIN_URL_WITH_KEY}`, {
-  params:{
-  q: form.elements.searchQuery.value,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-  page: `${page}`,
-  per_page: '40'}
-   });
-}
+const searchFormEL = document.querySelector('.search-form');
+const galleryEL = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 
-function createElements(response) {
-  gallery.insertAdjacentHTML('beforeend', response.data.hits.map(card).join(''));
-   lightbox.refresh();
-  if (response.data.hits.length < 40 && response.data.hits.length !== 0 || page === 13) {  
-    loader.classList.replace('load-more', 'visually-hidden');
-        setTimeout(() => {
-      Notiflix.Notify.info("Were sorry, but you've reached the end of search results.");
-        }, 1000);
-   }
-}
-  function addElafterScroll () {
-    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-    plusPage();
-    
-  }
-  }
- function auditAndBuild (response) {
-   if (response.data.totalHits === 0) {
-     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    } else {
-     Notiflix.Notify.success(`SHooray! We found ${response.data.totalHits} images.`);
-      loader.classList.replace('visually-hidden', 'load-more');
-   };
-   createElements(response)
-}
-  function moreBuild(response) {
-    createElements(response)
-     const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-})}
+searchFormEL.addEventListener('submit', onSearchPhoto);
+loadMore.addEventListener('click', onloadMore);
 
 
-function onSubmitClick(event) {
+function onSearchPhoto(event) {
   event.preventDefault();
-  gallery.innerHTML = '';
-  loader.classList.replace('load-more', 'visually-hidden');
+  galleryEL.innerHTML = '';
   page = 1;
-  if (form.elements.searchQuery.value.trim() === '') {
-    return;
-  }
-    getUser().then(res=>auditAndBuild(res)).catch(err=>console.log(err))
-}
+    value = event.currentTarget.elements.searchQuery.value;
+    get();
 
-function plusPage (){
-  page +=1;
-  getUser().then(res => moreBuild(res)).catch(err => console.log(err)) 
-  if (page === 13) {
-    window.removeEventListener('scroll', addElafterScroll)
+};
+
+function onloadMore() {
+  loadMore.classList.add('visually-hidden');
+  console.log(loadMore);
+  page = page + 1;
+  get();
+  return page
   }
-}
+
+async function get() {
+ await axios.get(`${BASE_URL}?${KEY}&${options}&q=${value}&page=${page}&per_page=${perPage}`)
+   .then(res => {
+     
+     if (res.data.hits.length === 0) {
+       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+       return
+     }
+
+     marcap(res);
+
+   })
+   .catch(() => {
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+       loadMore.classList.add('visually-hidden');
+   });
+ 
+};
 
 function marcap(object) {
   const marcap = object.data.hits.map(info => 
@@ -105,5 +84,3 @@ function marcap(object) {
       loadMore.classList.remove('visually-hidden');
         console.log(loadMore);  
 }
-window.addEventListener('scroll', addElafterScroll)
-form.addEventListener('submit', onSubmitClick);
